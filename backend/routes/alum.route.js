652f2e242d7155d7157f38f7347d5b5d1d1a504b
akashSingh <singhsahab2k3@gmail.com>
2025-07-29 23:30:00 +0530

    Restructured Code

--- backend/routes/alum.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const alumController = require('../controllers/alum.controller');
-
-
-router.get('/get-alums', alumController.getAlums);
-
-
-module.exports = router;
